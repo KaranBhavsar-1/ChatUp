3d9982871bed69cfe58c39e7386e000cb0b8d4f2
KaranBhavsar-1 <karanbhavsar0001@gmail.com>
2026-04-08 20:21:45 +0530

    Frontend setup done

---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+
+
+export const axiosInstance = axios.create({
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "https://chatup-mz5r.onrender.com/api",
+    withCredentials: true,
+})
