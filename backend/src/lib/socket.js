066b1db02013f8e7aeed561394bcc745b01a855a
KaranBhavsar-1 <karanbhavsar0001@gmail.com>
2026-04-11 19:15:53 +0530

    Cloudinary , image send implemented

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,38 +1,90 @@
+// import { Server } from "socket.io";
+// import http from "http";
+// import express from "express";
+// import { ENV } from "./env.js";
+
+// import {socketAuthMiddleware} from "../middleware/socket.Auth.Middleware.js";
+
+// const app = express()
+// const server = http.createServer(app)
+
+// const io = new Server(server,{
+//     cors: {
+//         origin: [ENV.CLIENT_URL],
+//         credentials:true,
+//     },
+// });
+
+// io.use(socketAuthMiddleware)
+
+// export function getReceiverSocketId(userId) {
+//     return userSocketMap[userId]
+// }
+// const userSocketMap = {};
+
+// io.on("connection", (socket) =>{
+//     console.log("A User Connected", socket.user.fullname)
+
+//     const userId = socket.userId;
+//     userSocketMap[userId] = socket.id
+
+//     io.emit("getOnlineUsers",Object.keys(userSocketMap));
+
+//     socket.on("Disconnects", ()=>{
+//         console.log("A User Disconnected" , socket.user.fullname)
+//         delete userSocketMap[userId]
+//         io.emit("getOnlineUsers",Object.keys(userSocketMap));
+
+//     })
+// })
+
+// export {io, app, server}
+
+
+
+
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
 import { ENV } from "./env.js";
+import { socketAuthMiddleware } from "../middleware/socket.Auth.Middleware.js"
 
-import {socketAuthMiddleware} from "../middleware/socket.Auth.Middleware.js";
+const app = express();
+const server = http.createServer(app);
 
-const app = express()
-const server = http.createServer(app)
-
-const io = new Server(server,{
-    cors: {
-        origin: [ENV.CLIENT_URL],
-        credentials:true,
-    },
+const io = new Server(server, {
+  cors: {
+    origin: [ENV.CLIENT_URL],
+    credentials: true,
+  },
 });
 
-io.use(socketAuthMiddleware)
+// apply authentication middleware to all socket connections
+io.use(socketAuthMiddleware);
 
-const userSocketMap = {};
+// we will use this function to check if the user is online or not
+export function getReceiverSocketId(userId) {
+  return userSocketMap[userId];
+}
 
-io.on("connection", (socket) =>{
-    console.log("A User Connected", socket.user.fullname)
+// this is for storig online users
+const userSocketMap = {}; // {userId:socketId}
 
-    const userId = socket.userId;
-    userSocketMap[userId] = socket.id
+io.on("connection", (socket) => {
+  console.log("A user connected", socket.user.fullName);
 
-    io.emit("getOnlineUsers",Object.keys(userSocketMap));
+  const userId = socket.userId;
+  userSocketMap[userId] = socket.id;
 
-    socket.on("Disconnects", ()=>{
-        console.log("A User Disconnected" , socket.user.fullname)
-        delete userSocketMap[userId]
-        io.emit("getOnlineUsers",Object.keys(userSocketMap));
+  // io.emit() is used to send events to all connected clients
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
-    })
-})
+  // with socket.on we listen for events from clients
+  socket.on("disconnect", () => {
+    console.log("A user disconnected", socket.user.fullName);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
 
-export {io, app, server}
+export { io, app, server };
