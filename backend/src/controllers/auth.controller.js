6ddc9c4954e8102c90dc18ac043729f2dae5c0da
KaranBhavsar-1 <karanbhavsar0001@gmail.com>
2026-04-11 20:39:11 +0530

    deployment ready -2

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -91,7 +91,9 @@ export const login = async (req,res)=>{
     }
 };
 export const logout = (_,res)=>{
-    res.cookie("jwt" , "", {maxAge:0})
+    res.cookie("jwt" , "", {maxAge:0,httpOnly: true,
+  secure: true,
+  sameSite: "None",})
     res.status(200).json({message:"Logged out Successfully"})
 };
 
