90d994c255cbd2aafea43cfa9ac7044a1a793300
KaranBhavsar-1 <karanbhavsar0001@gmail.com>
2026-04-14 16:49:43 +0530

    deployment submit -3

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://chatup-mz5r.onrender.com",
+    origin: [ENV.CLIENT_URL],
     credentials: true,
   },
 });
