fda7d8b9c03a9cd6153432a37a1146e044d5a42f
KaranBhavsar-1 <karanbhavsar0001@gmail.com>
2026-04-15 15:44:48 +0530

    deployment submit -11

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+axios.defaults.withCredentials = true;
 export const axiosInstance = axios.create({
     // baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
     // baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "https://chatup-mz5r.onrender.com/api",
