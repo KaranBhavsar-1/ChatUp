7433e33f206602894ad27e9d41420818d313bfc1
KaranBhavsar-1 <karanbhavsar0001@gmail.com>
2026-04-22 21:14:08 +0530

    added logo and managed size of main frame.

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -12,24 +12,40 @@ function ChatPage() {
   const { activeTab, selectedUser } = useChatStore();
 
   return (
-    <div className="relative w-full max-w-6xl h-[800px]">
-      <BorderAnimatedContainer>
-        {/* LEFT SIDE */}
-        <div className="w-80 bg-slate-800/50 backdrop-blur-sm flex flex-col">
-          <ProfileHeader />
-          <ActiveTabSwitch />
+    // <div className="relative w-full max-w-6xl h-[800px]">
+    //   {/* <BorderAnimatedContainer> */}
+    //     {/* LEFT SIDE */}
+    //     <div className="w-80 bg-slate-800/50 backdrop-blur-sm flex flex-col">
+    //       <ProfileHeader />
+    //       <ActiveTabSwitch />
 
-          <div className="flex-1 overflow-y-auto p-4 space-y-2">
-            {activeTab === "chats" ? <ChatsList /> : <ContactList />}
-          </div>
-        </div>
+    //       <div className="flex-1 overflow-y-auto p-4 space-y-2">
+    //         {activeTab === "chats" ? <ChatsList /> : <ContactList />}
+    //       </div>
+    //     </div>
 
-        {/* RIGHT SIDE */}
-        <div className="flex-1 flex flex-col bg-slate-900/50 backdrop-blur-sm">
-          {selectedUser ? <ChatContainer /> : <NoConversationPlaceholder />}
-        </div>
-      </BorderAnimatedContainer>
+    //     {/* RIGHT SIDE */}
+    //     <div className="flex-1 flex flex-col bg-slate-900/50 backdrop-blur-sm">
+    //       {selectedUser ? <ChatContainer /> : <NoConversationPlaceholder />}
+    //     </div>
+    //   {/* </BorderAnimatedContainer> */}
+    // </div>
+  <div className="relative w-full max-w-6xl h-[calc(100vh-80px)] mx-auto flex overflow-hidden">
+  {/* LEFT SIDE */}
+  <div className="w-72 md:w-80 bg-slate-800/50 backdrop-blur-sm flex flex-col">
+    <ProfileHeader />
+    <ActiveTabSwitch />
+
+    <div className="flex-1 overflow-y-auto p-4 space-y-2">
+      {activeTab === "chats" ? <ChatsList /> : <ContactList />}
     </div>
+  </div>
+
+  {/* RIGHT SIDE */}
+  <div className="flex-1 flex flex-col bg-slate-900/50 backdrop-blur-sm">
+    {selectedUser ? <ChatContainer /> : <NoConversationPlaceholder />}
+  </div>
+</div>
   );
 }
 export default ChatPage;
